4b5780d88c30c67455e84744dfc2559a3b6aea04
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-06-01 11:51:16 -0700

    Initial commit

---
+++ model.py
@@ -0,0 +1,126 @@
+""" Models for eVentry app """
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """ A user """
+
+    # creating a users table
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(30))
+    lname = db.Column(db.String(30))
+    email = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} fname={self.fname} lname={self.lname} email={self.email}>'
+
+
+class Theme(db.Model):
+    """ An event theme """
+
+    # creating a themes table
+    __tablename__ = "themes"
+
+    theme_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(30))
+    color = db.Column(db.String(30))
+
+    def __repr__(self):
+        return f'<Theme theme_id={self.theme_id} name={self.name} color={self.color}>'
+
+
+class Image(db.Model):
+    """ An image """
+
+    # creating a images table
+    __tablename__ = "images"
+
+    img_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    url = db.Column(db.String(30))
+
+    def __repr__(self):
+        return f'<Image img_id={self.img_id} url={self.url}>'
+
+
+class Event(db.Model):
+    """ An event """
+
+    # creating an events table
+    __tablename__ = "events"
+
+    event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    category = db.Column(db.String(30))
+    theme_id = db.Column(db.Integer, db.ForeignKey('themes.theme_id'))
+    img_id = db.Column(db.Integer, db.ForeignKey('images.img_id'))
+
+    user = db.relationship('User', backref='events')
+    theme = db.relationship('Theme', backref='events')
+    imgs = db.relationship('Image', backref='event')
+
+    def __repr__(self):
+        return f'<Event event_id={self.event_id} category={self.category}>'
+
+
+class EventsItems(db.Model):
+    """ Association table for Events and Items """
+
+    # creating an events_items table
+    __tablename__ = "events_items"
+
+    events_items = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.item_id'))
+
+    event = db.relationship('Event', backref='event_item')
+    item = db.relationship('Item', backref='event_item')
+
+    def __repr__(self):
+        return f'<EventsItems events_items={self.events_items} event_id={self.event_id} item_id={self.item_id}>'
+
+
+class Item(db.Model):
+    """ An event item """
+
+    # creating an items table
+    __tablename__ = "items"
+
+    item_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    description = db.Column(db.Text)
+    link = db.Column(db.String)
+    img_id = db.Column(db.Integer, db.ForeignKey('images.img_id'))
+
+    imgs = db.relationship('Image', backref='item')
+
+    def __repr__(self):
+        return f'<Item item_id={self.item_id} description={self.description}>'
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///eventry', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    # from server import app
+
+    app = Flask(__name__)
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
